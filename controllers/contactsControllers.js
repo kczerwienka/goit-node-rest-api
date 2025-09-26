8dcee767af1e2a833ae6a2a54b69f6a584e8208a
kczerwienka <57370801+kczerwienka@users.noreply.github.com>
2025-09-26 19:37:59 +0200

    x

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,83 @@
-import contactsService from "../services/contactsServices.js";
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+{
+}
 
-export const getOneContact = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  const contacts = await listContacts();
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      contacts,
+    },
+  });
+};
 
-export const deleteContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+  const { id } = req.params;
+  const [contact] = await getContactById(id);
+  if (!contact) {
+    res.status(404).json({
+      status: "error",
+      code: 404,
+      message: `Contact with id=${id} not found`,
+    });
+  }
+  res.json({
+    status: "success",
+    code: 200,
+    data: { contact },
+  });
+};
 
-export const createContact = (req, res) => {};
+export const deleteContact = async (req, res) => {
+  const { id } = req.params;
+  const [contact] = await removeContact(id);
+  if (!contact) {
+    res.status(404).json({
+      status: "error",
+      code: 404,
+      message: `Contact with id=${id} not found`,
+    });
+  }
+  res.json({
+    status: "success",
+    code: 200,
+    data: { contact },
+  });
+};
 
-export const updateContact = (req, res) => {};
+export const createContact = async (req, res) => {
+  const { name, email, phone } = req.params;
+  const contact = await addContact(name, email, phone);
+  res.status(201).json({
+    status: "success",
+    code: 201,
+    data: { contact },
+  });
+};
+
+export const updateContact = (req, res) => {
+  const { id } = req.params[0];
+  const [contact] = deleteContact(id);
+  if (!contact) {
+    res.status(404).json({
+      status: "error",
+      code: 404,
+      message: `Contact with id=${id} not found`,
+    });
+  }
+  const { name, email, phone } = req.params;
+  const updatedContact = addContact(id, name, email, phone);
+  res.json({
+    status: "success",
+    code: 200,
+    data: { updatedContact },
+  });
+};
